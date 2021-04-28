42cfcf8af4bfb4933a86bf06fce9778ba5a57a0d
A Wahid Saphadi <awahid.safhadi@gmail.com>
2021-04-28 22:11:00 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,144 @@
+function alif() {
+    var audio = new Audio('audio/hijaiyah/alif.mp3');
+    audio.play();
+}
+
+function ba() {
+    var audio = new Audio('audio/hijaiyah/ba.mp3');
+    audio.play();
+}
+
+function ta() {
+    var audio = new Audio('audio/hijaiyah/ta.mp3');
+    audio.play();
+}
+
+function tsa() {
+    var audio = new Audio('audio/hijaiyah/tsa.mp3');
+    audio.play();
+}
+
+function jim() {
+    var audio = new Audio('audio/hijaiyah/jim.mp3');
+    audio.play();
+}
+
+function Ha() {
+    var audio = new Audio('audio/hijaiyah/Ha (besar).mp3');
+    audio.play();
+}
+
+function kho() {
+    var audio = new Audio('audio/hijaiyah/kho.mp3');
+    audio.play();
+}
+
+function dal() {
+    var audio = new Audio('audio/hijaiyah/dal.mp3');
+    audio.play();
+}
+
+function dza() {
+    var audio = new Audio('audio/hijaiyah/dza.mp3');
+    audio.play();
+}
+
+function ro() {
+    var audio = new Audio('audio/hijaiyah/ro.mp3');
+    audio.play();
+}
+
+function zay() {
+    var audio = new Audio('audio/hijaiyah/zay.mp3');
+    audio.play();
+}
+
+function sin() {
+    var audio = new Audio('audio/hijaiyah/sin.mp3');
+    audio.play();
+}
+
+function syin() {
+    var audio = new Audio('audio/hijaiyah/syin.mp3');
+    audio.play();
+}
+
+function shod() {
+    var audio = new Audio('audio/hijaiyah/shod.mp3');
+    audio.play();
+}
+
+function dhod() {
+    var audio = new Audio('audio/hijaiyah/dhod.mp3');
+    audio.play();
+}
+
+function tho() {
+    var audio = new Audio('audio/hijaiyah/tho.mp3');
+    audio.play();
+}
+
+function dhzo() {
+    var audio = new Audio('audio/hijaiyah/dhzo.mp3');
+    audio.play();
+}
+
+function ain() {
+    var audio = new Audio('audio/hijaiyah/ain.mp3');
+    audio.play();
+}
+
+function ghoin() {
+    var audio = new Audio('audio/hijaiyah/ghoin.mp3');
+    audio.play();
+}
+
+function fa() {
+    var audio = new Audio('audio/hijaiyah/fa.mp3');
+    audio.play();
+}
+
+function qof() {
+    var audio = new Audio('audio/hijaiyah/qof.mp3');
+    audio.play();
+}
+
+function kaf() {
+    var audio = new Audio('audio/hijaiyah/kaf.mp3');
+    audio.play();
+}
+
+function lam() {
+    var audio = new Audio('audio/hijaiyah/lam.mp3');
+    audio.play();
+}
+
+function mim() {
+    var audio = new Audio('audio/hijaiyah/mim.mp3');
+    audio.play();
+}
+
+function nun() {
+    var audio = new Audio('audio/hijaiyah/nun.mp3');
+    audio.play();
+}
+
+function ha() {
+    var audio = new Audio('audio/hijaiyah/ha.mp3');
+    audio.play();
+}
+
+function waw() {
+    var audio = new Audio('audio/hijaiyah/waw.mp3');
+    audio.play();
+}
+
+function ya() {
+    var audio = new Audio('audio/hijaiyah/ya.mp3');
+    audio.play();
+}
+
+function hamzah() {
+    var audio = new Audio('audio/hijaiyah/hamzah.mp3');
+    audio.play();
+}
---
+++ js/nav.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Activate sidebar nav
+    const sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+    loadNav();
+
+    function loadNav() {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status != 200) return;
+
+                // Muat daftar tautan menu
+                document.querySelectorAll(".topnav, .sidenav").forEach(function (e) {
+                    e.innerHTML = xhttp.responseText;
+                });
+
+                // Daftarkan event listener untuk setiap tautan menu
+                document.querySelectorAll(".sidenav a, .topnav a").forEach(function (e) {
+                    e.addEventListener("click", function (event) {
+                        // Tutup sidenav
+                        const sidenav = document.querySelector(".sidenav");
+                        M.Sidenav.getInstance(sidenav).close();
+
+                        // Muat konten halaman yang dipanggil
+                        page = event.target.getAttribute("href").substr(1);
+                        loadPage(page);
+                    });
+                });
+            }
+        };
+        xhttp.open("GET", "nav.html", true);
+        xhttp.send();
+    }
+
+    // Load page content
+    let page = window.location.hash.substr(1);
+    if (page == "") page = "home";
+    loadPage(page);
+
+    function loadPage(page) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                const content = document.querySelector("#body-content");
+                if (this.status == 200) {
+                    content.innerHTML = xhttp.responseText;
+                } else if (this.status == 404) {
+                    content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+                } else {
+                    content.innerHTML = "<p>Halaman tidak dapat diakses.</p>";
+                }
+            }
+        };
+        xhttp.open("GET", "pages/" + page + ".html", true);
+        xhttp.send();
+    }
+});
