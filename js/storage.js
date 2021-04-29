2c9acf1ec741d2d35314d5a96f9f122468ee0af3
A Wahid Saphadi <awahid.safhadi@gmail.com>
2021-04-29 19:29:08 +0800

    update fitur alquran dan fix tampilan

---
+++ js/storage.js
@@ -0,0 +1,36 @@
+const KEY_TES = "tes";
+const KEY_BOOKMARK = "bookmark";
+
+function checkForStorage() {
+    return typeof (Storage) !== "undefined";
+}
+
+const set = (data) => {
+    localStorage.setItem(KEY_BOOKMARK, JSON.stringify(data));
+
+};
+
+const get = () => {
+    if (checkForStorage) {
+        return JSON.parse(localStorage.getItem(KEY_BOOKMARK)) || [];
+    } else {
+        return [];
+    }
+};
+
+
+const addData = (data)=>{
+    storageData = get();
+    storageData.unshift(data);
+    set(storageData);
+};
+
+const readData = () => {
+    return get();
+};
+
+const deleteData = (i) =>{
+    storageData = get();
+    storageData.splice(i);
+    set(storageData);
+};
